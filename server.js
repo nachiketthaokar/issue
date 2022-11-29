0d50f935d65657675703bebc376d506004232697
nachiketthaokar <75263249+nachiketthaokar@users.noreply.github.com>
2022-11-29 16:18:58 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const app = express()
+const server = require("http").Server(app)
+
+app.get("/",(req,res) => {
+    res.status(200).send("hello World")
+})      
+server.listen(30,30)
